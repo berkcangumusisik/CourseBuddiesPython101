a2be3598760df4544e8274c0f7eb84d8fe96be18
Berkcan Gümüşışık <gumusisikberkcan@gmail.com>
2021-08-26 13:50:03 +0300

    26.08.2021 13.49

--- Adam Asmaca/hangman.py
+++ Adam Asmaca/hangman.py
@@ -1,24 +1,19 @@
-""" 
-* Kelime havuzu oluşturulacak. Kategoriler belirlenecek.
-* Resimler halledilecek.
-* Kelime dosyasından kelime alınacak
-* Kelime uzunluğu hesaplanacak
-* Can sayısı vericez.
-* Kullanıcıya hoş geldiniz metni ve hangman logosu eklenecek.
-* Kelimelerin uzunluğu en az 7 olsun.
-* Kategoriye göre seçim yapılacak.
-* Kelime harflerinin olacağı liste oluşturulup ilk olarak _ olacak sonra bulunan harf yazılacak.
-* oyun_bitti_mi adında bir değişken oluşturulup while döngüsüne sokulacak.
-* Kullanıcıdan harf istenecek.
-* Bilinen harf _ ile değiştirilecek. Eğer harf bilemediyse canından -1 azalacak.
-* Can 0 olunca oyun biter. Kaybettiniz yazısı eklenir.
-* Kazanınca tebrikler kazandınız yazısı
-* bir harfe basılınca oyundan çıkabilir.
-"""
 import random
 from hangmanArt import stages, logo
 from hangmanWords import canlilar, ulkeler, nesneler, kelimeler
 
+def change_letter(kelime,rastgele_kelime,letter):
+    k=rastgele_kelime
+    while letter in k:
+        s = list(kelime)
+        k = "".join(k)
+        x = k.find(letter)
+        k = list(k)
+        k[x]="*"
+        s[x] = letter
+        kelime = "".join(s)
+
+    return kelime
 
 print(logo)
 print("------------------------ADAM ASMACA OYUNUNA HOŞ GELDİNİZ-----------------------")
@@ -31,46 +26,63 @@ print("""Kategoriler
 3. Nesneler
 4. Rastgele
 """)
+
 kategori = int(input("Bir kategori numarası giriniz: "))
+
 if kategori == 1:
-    secilenKategori = random.choice(canlilar)
-    kelimeUzunlugu = len(secilenKategori)
+    rastgele_kelime = random.choice(canlilar)
 elif kategori == 2:
-    secilenKategori = random.choice(ulkeler)
-    kelimeUzunlugu = len(secilenKategori)
+    rastgele_kelime = random.choice(ulkeler)
 elif kategori == 3:
-    secilenKategori = random.choice(nesneler)
-    kelimeUzunlugu = len(secilenKategori)
+    rastgele_kelime = random.choice(nesneler)
 elif kategori == 4:
-    secilenKategori = random.choice(kelimeler)
-    kelimeUzunlugu = len(secilenKategori)
+    rastgele_kelime = random.choice(kelimeler)
 else:
     print("Yalnış kategori seçtiniz. 1- 4 arasında değer giriniz.")
 
 
 kelime = ""
-for i in secilenKategori:
+for i in rastgele_kelime:
     if i != " ":
         kelime += "_"
     else:
         kelime += " "
 
+guess_list=[]
+print(kelime)
+
 while not oyunBittiMi:
     harf = input("Bir harf girişi yapınız:").lower()
-    if harf in kelime:
+
+    if harf in guess_list:
         print(f"Zaten {harf} harfini tahmin etmiştiniz.")
-    for pozisyon in range(kelimeUzunlugu):
-        letter = secilenKategori[pozisyon]
-        if letter == harf:
-            kelime[pozisyon] = letter
-        print(f"{' '.join(kelime)}")
-        if harf not in kelime:
-            print(f"{harf} harfi kelime içerisinde bulunmuyor.")
-            can -= 1
-            if can == 0:
-                oyunBittiMi = True
-                print("Oyunu kaybettiniz. Üzgünüz :(")
+        print(kelime)
+
+        pass
+    else:
+        for i,letter in enumerate(rastgele_kelime):
+            if letter == harf:
+                kelime=change_letter(kelime,rastgele_kelime,letter)
+                print(kelime)
+                break
+
+            elif harf not in rastgele_kelime:
+                print(f"{harf} harfi kelime içerisinde bulunmuyor.")
+                print(kelime)
+                can -= 1
+                if can == 0:
+                    oyunBittiMi = True
+                    print("Oyunu kaybettiniz. Bilinmesi gereken kelime {} olacaktı. Üzgünüz :(".format(rastgele_kelime))
+                    break
+                else:
+                    break
         if not "_" in kelime:
-            oyunBittiMi = True
-            print("Oyunu kazandınız. Tebrikler :)")
+                oyunBittiMi = True
+                print("Oyunu kazandınız. Tebrikler :)")
+                break
+        guess_list+=harf
         print(stages[can])
+
+
+
+
